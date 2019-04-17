b15e7ec72d3dcc70c405a057c1a855a597ab9d6b
Justin Barclay <justincbarclay@gmail.com>
2019-04-17 11:58:10 -0400

    Add script to convert move group servers to csv format
    Docs can be found at:
    https://guides.tidalmg.com/exporting-servers-to-csv.html

---
+++ move_group_to_csv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+access_token=$(curl -H Content-Type\:\ application/json -XPOST https\://$subdomain.tidalmg.com/api/v1/authenticate -d \{\"username\"\:\ \"$username\"\,\ \"password\"\:\ \"$password\"\} | jq '.access_token' | sed 's/"//g')
+
+move_group=$(curl -H Authorization\:\ \"Bearer\ $access_token\" -H Content-Type\:\ application/json -XGET https\://$subdomain.tidalmg.com/api/v1/move_groups/$move_group_id | jq '.servers')
+
+echo "hostname, username, password, domain"
+echo $move_group | jq -M -r '.[] | [.host_name, .username, .password, .domain] | @csv'
