fbb9ef85ba19bae9e7ecf3f23361c760ec48b732
Philip Rees <philip.rees@tidalmigrations.com>
2020-11-10 10:03:40 -0500

    Adjust move group script to use custom fields

--- move_group_to_csv.sh
+++ move_group_to_csv.sh
@@ -4,4 +4,4 @@ access_token=$(curl -H Content-Type\:\ application/json -XPOST https\://$subdoma
 move_group=$(curl -H Authorization\:\ \"Bearer\ $access_token\" -H Content-Type\:\ application/json -XGET https\://$subdomain.tidalmg.com/api/v1/move_groups/$move_group_id | jq '.servers')
 
 echo "hostname, username, password, domain"
-echo $move_group | jq -M -r '.[] | [.host_name, .username, .password, .domain] | @csv'
+echo $move_group | jq -M -r '.[] | .host_name as $h | .custom_fields | [$h, .username, .password, .domain] | @csv'
