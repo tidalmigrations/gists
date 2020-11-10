9ae1869b0864181de26288a3c9aab04da7aad101
Justin Barclay <justincbarclay@gmail.com>
2020-11-10 14:12:04 -0700

    Add powershell version of move_group_to_csv.ps1

---
+++ move_group_to_csv.ps1
@@ -0,0 +1,27 @@
+# $username = ""
+# $password = ""
+# $move_group_id = ""
+# $subdomain = ""
+
+$body = @{
+    "username" = $username
+    "password" = $password
+}
+
+$access_token = Invoke-WebRequest -Uri "https://${subdomain}.tidalmg.com/api/v1/authenticate" -Method "Post" `
+                                  -Body ($body|ConvertTo-Json) -ContentType "application/json" `
+                | ConvertFrom-Json `
+                | Select -ExpandProperty access_token
+
+$auth = @{"Authorization" = "Bearer ${access_token}";}
+
+$servers = Invoke-WebRequest -Uri "https://${subdomain}.tidalmg.com/api/v1/move_groups/${move_group_id}" -Method "Get" `
+                             -Headers $auth -ContentType "application/json" `
+           | ConvertFrom-Json `
+           | Select-Object -Expand servers `
+           | Select-Object -Property host_name,
+                                     @{Name = 'username';   Expression = { $_.custom_fields.username }}, `
+                                     @{Name = 'password';   Expression = { $_.custom_fields.password }}, `
+                                     @{Name = 'domain';   Expression = { $_.custom_fields.domain }} `
+
+echo $servers | ConvertTo-Csv
