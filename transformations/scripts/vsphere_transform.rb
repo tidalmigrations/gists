23616bb567d939106b31fa3baebf50a9533fb805
mario mejia <jmariomejiap@gmail.com>
2022-10-28 17:28:24 -0300

    update vsphere transform script to match new improvements

--- transformations/scripts/vsphere_transform.rb
+++ transformations/scripts/vsphere_transform.rb
@@ -7,21 +7,32 @@ def transform(input)
   data = {servers: []}
   json["VirtualMachines"].each do |vm|
     props = {}
+    
     config = vm["Summary"]["Config"]
+    stats = vm["Summary"]["QuickStats"]
+    storage = vm["Summary"]["Storage"]
+    hostruntime = vm["Summary"]["Runtime"]["Host"]
     run = vm["Runtime"]
+    guest = vm["Guest"]
+
+    props[:fqdn] = guest["Hostname"]
+    props[:operating_system] = guest["GuestFullName"]
     props[:host_name] = config["Name"]
     props[:description] = config["Annotation"]
     props[:assigned_id] = config["Uuid"]
-    props[:ram_allocated_gb] = config["MemorySizeMB"] / 1024
+    props[:ram_allocated_gb] = config["MemorySizeMB"].to_f / 1024    
+    props[:ram_used_gb] = stats["GuestMemoryUsage"].to_f / 1024
     props[:cpu_count] = config["NumCpu"]
-    props[:ram_used_gb] = run["MaxMemoryUsage"] / 1024
+    props[:storage_used_gb] = storage["Committed"].to_f / 2**(10*3)
+    props[:storage_allocated_gb] = (storage["Committed"].to_f + storage["Uncommitted"].to_f) / 2**(10*3)
     props[:virtual] = true
+    props[:cpu_name] = hostruntime["host"] ? hostruntime["host"]["Summary"]["Hardware"]["CpuModel"] : nil
+    props[:operating_system_version] = nil
+    props[:ip_addresses] = guest["Net"].map do |net| net["IpAddress"]  end
 
-    stats = vm["Summary"]["QuickStats"]
     custom = {status:  stats["GuestHeartbeatStatus"],
               product: config["Product"] ? config["Product"]["Name"] : false }
     props[:custom_fields] = custom
-    props[:environment] = 'Development'
 
     data[:servers].push props
   end
