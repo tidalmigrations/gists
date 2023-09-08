711bc967b73027bcd71904a9733311d7dbc948ba
Ravi Gaddam <ravi.gaddam@tidalmigrations.com>
2023-09-08 16:46:24 -0400

    db script updated to handle servers and dbs, and association

--- azure_dbs.rb
+++ azure_dbs.rb
@@ -106,7 +106,28 @@ module HttpUtil
     end
   end
   
-
+  module AzureDBServer
+    include HttpUtil
+    include JSON
+    
+    SERVER_API_VERSION = "2022-02-01-preview"
+    
+    def format_db_server_for_tidal(server)
+      properties = server["properties"] || {}
+      tags = (server["tags"] && server["tags"]["use"]) || server[:tags]&.split(": ")&.last
+    
+      STDERR.puts "Raw server data: #{server.inspect}"
+      {
+          host_name: server["name"] || server[:name],
+          fqdn: properties["fullyQualifiedDomainName"],
+          environment: tags,
+          custom_fields: {
+              location: server["location"]
+          }
+      }.compact
+    end         
+  end
+  
   module AzureDB
     include HttpUtil
     include JSON
@@ -148,15 +169,20 @@ module HttpUtil
           "Authorization" => "Bearer #{get_token}"
         }
       )
-      result = response_handler(api_name: "Azure Database Servers", response: response)["value"].map do |server|
+      servers_data = response_handler(api_name: "Azure Database Servers", response: response)["value"]
+      # servers_data.each { |server| STDERR.puts "Detailed DB Server Data: #{server.inspect}" } 
+      servers_data.each { |server| } 
+      result = servers_data.map do |server|
         {
           name: server["name"],
-          tags: server["tags"]&.map { |k, v| "#{k}: #{v}" }&.join(', ')
+          tags: server["tags"]&.map { |k, v| "#{k}: #{v}" }&.join(', '),
+          detailed_data: server 
         }
-      end
-      STDERR.puts "DB Servers Result for #{resource_group}: #{result.inspect}"
+      end      
+      # STDERR.puts "DB Servers Result for #{resource_group}: #{result.inspect}"
       result
     end
+    
     
     def list_databases_by_server(subscription, resource_group, server_name)
       path = "/subscriptions/#{subscription}/resourceGroups/#{resource_group}/providers/Microsoft.Sql/servers/#{server_name}/databases"
@@ -168,21 +194,45 @@ module HttpUtil
         }
       )
       result = response_handler(api_name: "Azure Databases By Server", response: response)["value"].map do |db|
+        # STDERR.puts "Databases retrieved: #{result.inspect}"
         {
           name: db["name"],
           tags: db["tags"]&.map { |k, v| "#{k}: #{v}" }&.join(', '),
-          max_size_bytes: db.dig("properties", "maxSizeBytes") || 0
+          max_size_bytes: db.dig("properties", "maxSizeBytes") || 0,
+          location: db["location"]
         }
       end
       STDERR.puts "DBs for Server #{server_name} in #{resource_group}: #{result.inspect}"
       result
-    end
+    end    
   end
 
 class DBFetcher
   extend AzureDB
+  extend AzureDBServer
 
-  def self.pull_from_azure_db
+  def self.sync_to_tidal_portal(data, type)
+    file_path = "/tmp/tidal_#{type}_data.json"
+    File.write(file_path, JSON.dump({ "#{type}": [data] }))
+
+    # STDERR.puts "Data being sent to Tidal: #{JSON.dump({ "#{type}": [data] })}"
+
+    command_output = `tidal request -X POST "/api/v1/#{type}/sync" #{file_path}`
+    
+    # STDERR.puts "Tidal Sync Output: #{command_output}"
+  
+    match = command_output.match(/"id":\s*(\d+)/)
+    if match
+      return match[1]
+    else
+      raise "Failed to capture ID from Tidal sync output."
+    end
+end
+
+  
+
+  def self.pull_from_azure_server_and_db
+    all_servers = []
     all_dbs = []
   
     STDERR.puts "Fetching subscriptions..."
@@ -199,36 +249,51 @@ class DBFetcher
         db_servers = list_db_servers(subscription, resource_group)
 
         db_servers.each do |server|
-          STDERR.puts "Fetching databases for server #{server[:name]}..."
-          dbs = list_databases_by_server(subscription, resource_group, server[:name])
+          detailed_server_data = server[:detailed_data]
+          formatted_server = format_db_server_for_tidal(detailed_server_data)
+          # STDERR.puts "Syncing server #{server["name"]} to Tidal..."
+          server_id = sync_to_tidal_portal(formatted_server, "servers")
+
+          # STDERR.puts "Server ID in Tidal: #{server_id}"
+
+          begin
+            dbs = list_databases_by_server(subscription, resource_group, server[:name])
+            # STDERR.puts "Databases before adding to all_dbs: #{dbs.inspect}"
+          rescue => e
+            STDERR.puts "Error fetching databases for server #{server[:name]}: #{e.message}"
+          end
+          
+
           all_dbs.concat(dbs.map do |db|
             {
-                server_name: server[:name],
-                database_name: db[:name],
-                max_size_bytes: db[:max_size_bytes],
-                tags: db[:tags]
+              server_name: server[:name], 
+              database_name: db[:name],
+              max_size_bytes: db[:max_size_bytes],
+              tags: db[:tags],
+              host_name: server_id,
+              location: db[:location] 
             }
-        end)
+          end)
         end
       end
     end
   
     formatted_dbs = all_dbs.map do |db|
-      STDERR.puts "Transforming DB: #{db.inspect}"
-      env_tag = db[:tags]&.split(', ')&.find { |tag| tag.start_with?('environment: ') }
-      environment = env_tag ? env_tag.split(': ')[1] : "production"
+      # STDERR.puts "Transforming DB: #{db.inspect}"
+      # env_tag = db[:tags]&.split(', ')&.find { |tag| tag.start_with?('environment: ') }
+      # environment = env_tag ? env_tag.split(': ')[1] : "production"
 
       {
         name: db[:database_name],
-        server: db[:server_name],
-        tags: db[:tags],
         database_engine: "SQL Server",
         database_size_mb: (db[:max_size_bytes] / (1024**2)).round,
         database_path: "N/A",
         description: "Azure SQL Database",
-        environment: environment,
+        environment: db[:tags],
+        server: { host_name: db[:server_name] },
         custom_fields: {
-          technologies: "Azure SQL"
+          technologies: "Azure SQL",
+          location: db[:location]
         }
       }
     end
@@ -238,7 +303,7 @@ class DBFetcher
 
   case ARGV[0]
   when nil
-    pull_from_azure_db
+    pull_from_azure_server_and_db
   when "-h"
     puts <<~EOT
       Azure Database Fetching Menu:
