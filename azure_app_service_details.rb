b6277b46a206aa1a1522504e11a7eebafc3b8c26
Philip Rees <phil@tidalcloud.com>
2023-12-08 15:08:04 -0500

    Improve code structure, decouple dependency on other modules

--- azure_app_service_details.rb
+++ azure_app_service_details.rb
@@ -1,41 +1,54 @@
 #!/bin/env ruby
+module AzureAppServiceApi
+  require "net/http"
 
-require "./azure_migrate.rb"
-require "./azure_vms.rb"
-require 'fileutils'
+  def list_resource_groups(subscription)
+    azure_request("/subscriptions/#{subscription}/resourcegroups", :get, "2023-07-01")["value"]
+  end
 
-module AzureAppServiceDetails
-  include AzureMigrate
-  include AzureAppService
-
-  # to get resource groups
-  include AzureVM
+  def list_app_services(subscription, resource_group_name)
+    azure_request("/subscriptions/#{subscription}/resourceGroups/#{resource_group_name}/providers/Microsoft.Web/sites",
+                  :get, "2022-09-01")["value"]
+  end
 
   def list_service_plans(subscription_id)
     azure_request "/subscriptions/#{subscription_id}/providers/Microsoft.Web/serverfarms"
   end
 
   def get_app_service(subscription, name, resource_group_name)
-    azure_request "/subscriptions/#{subscription}/resourceGroups/#{resource_group_name}/providers/Microsoft.Web/sites/#{name}"
-  end
-
-  def list_app_configs(subscription, resource_group_name, app_name)
-    azure_request "/subscriptions/#{subscription}/resourceGroups/#{resource_group_name}/providers/Microsoft.Web/sites/#{app_name}/config/web"
-    # returns similar result but appSettings empty as well
-    #azure_request "/subscriptions/#{subscription}/resourceGroups/#{resource_group_name}/providers/Microsoft.Web/sites/#{app_name}/config"
-  end
-
-  def app_settings(subscription, resource_group_name, app_name)
-    azure_request "/subscriptions/#{subscription}/resourceGroups/#{resource_group_name}/providers/Microsoft.Web/sites/#{app_name}/config/appsettings/list"
+    azure_request "/subscriptions/#{subscription}/resourceGroups/#{resource_group_name}/providers/" \
+                  "Microsoft.Web/sites/#{name}"
   end
 
   def connection_strings(subscription, resource_group_name, app_name)
-    azure_request "/subscriptions/#{subscription}/resourceGroups/#{resource_group_name}/providers/Microsoft.Web/sites/#{app_name}/config/connectionstrings/list", :post
+    azure_request "/subscriptions/#{subscription}/resourceGroups/#{resource_group_name}/providers/" \
+                  "Microsoft.Web/sites/#{app_name}/config/connectionstrings/list", :post
+  end
+
+  # currently not returning needed Appsettings values
+  def list_app_configs(subscription, resource_group_name, app_name)
+    azure_request "/subscriptions/#{subscription}/resourceGroups/#{resource_group_name}/providers/" \
+                  "Microsoft.Web/sites/#{app_name}/config/web"
+    # returns similar result but appSettings empty as well
+    # azure_request "/subscriptions/#{subscription}/resourceGroups/#{resource_group_name}/providers/" \
+    #               "Microsoft.Web/sites/#{app_name}/config"
+  end
+
+  # currently not returning needed Appsettings values
+  def app_settings(subscription, resource_group_name, app_name)
+    azure_request "/subscriptions/#{subscription}/resourceGroups/#{resource_group_name}/providers/" \
+                  "Microsoft.Web/sites/#{app_name}/config/appsettings/list"
   end
 
   # redundant, not needed, URL for this resource returned with app service object
+  # also note, 'server farm' is analogous for 'service plan'
   def server_farms(subscription_id, group_name, app_service_plan_name)
-    azure_request "/subscriptions/#{subscription_id}/resourceGroups/#{group_name}/providers/Microsoft.Web/serverfarms/#{app_service_plan_name}"
+    azure_request "/subscriptions/#{subscription_id}/resourceGroups/#{group_name}/providers/" \
+                  "Microsoft.Web/serverfarms/#{app_service_plan_name}"
+  end
+
+  def app_service_service_plan(app_service)
+    azure_request app_service["properties"]["serverFarmId"]
   end
 
   # empty as of now with current demo setup
@@ -43,22 +56,21 @@ module AzureAppServiceDetails
     azure_request "/subscriptions/#{sub}/providers/Microsoft.Web/hostingEnvironments"
   end
 
-  # can't query without worker_pool name value
+  # can only query with a worker_pool name value
   def private_endpoint_connections(sub, resource_group, name)
-    azure_request "/subscriptions/#{sub}/resourceGroups/#{resource_group}/providers/Microsoft.Web/hostingEnvironments/#{name}/privateEndpointConnections"
+    azure_request "/subscriptions/#{sub}/resourceGroups/#{resource_group}/providers/" \
+                  "Microsoft.Web/hostingEnvironments/#{name}/privateEndpointConnections"
   end
 
-  def azure_request(path, method = :get)
+  def azure_request(path, method = :get, version = "2022-03-01")
     JSON.parse(make_request(method:       method,
-                            path:         "#{base_url}#{path}",
+                            path:         "https://management.azure.com#{path}",
                             body:         nil,
-                            query_params: { "api-version": "2022-03-01" },
-                            headers:      { "Authorization" => "Bearer #{get_token}" }).response.body)
+                            query_params: { "api-version": version },
+                            headers:      { "Authorization" => "Bearer #{ENV.fetch('AZURE_TOKEN')}" }).response.body)
   end
 
   def make_request(method:,path:, query_params: {}, body: {}, headers: {}, form: [], ssl: true, timeout: 60, basic_auth: [])
-    raise ArgumentError, "Must provide a valid method: #{valid_methods}" unless valid_methods.include? method.downcase
-
     uri, http = get_uri_http(path:         path,
                              ssl:          ssl,
                              query_params: query_params)
@@ -77,15 +89,30 @@ module AzureAppServiceDetails
     http.request(request)
   end
 
+  def get_uri_http(path:, query_params: nil, ssl: true)
+    uri = URI(path)
+    uri.query = URI.encode_www_form query_params if query_params
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = ssl
+    [uri, http]
+  end
+end
+
+module AzureAppServiceDetails
+  require "fileutils"
+  require "json"
+  include AzureAppServiceApi
+
   def all_app_services(subscription)
     list_resource_groups(subscription).map do |resource_group|
-      list_app_services(subscription, resource_group)
+      list_app_services(subscription, resource_group["name"])
     end.flatten
   end
 
   def app_service_details(sub, app_service)
-    app_service["service_plan"] = azure_request app_service["properties"]["serverFarmId"]
-    app_service["app_connection_strings"] = connection_strings(sub, app_service["properties"]["resourceGroup"], app_service["name"])
+    app_service["service_plan"] = app_service_service_plan(app_service)
+    app_service["app_connection_strings"] = connection_strings(sub, app_service["properties"]["resourceGroup"],
+                                                               app_service["name"])
     app_service["app_configs"] = list_app_configs(sub, app_service["properties"]["resourceGroup"], app_service["name"])
     app_service
   end
@@ -132,16 +159,13 @@ module AzureAppServiceDetails
   end
 
   def output_app_service(subscription, name)
-    get_details_and_output(subscription, all_app_services(subscription).select { |app_service| app_service["name"] == name }.first)
+    get_details_and_output(subscription, all_app_services(subscription)
+                                         .select { |app_service| app_service["name"] == name }.first)
   end
+end
 
-  def output_first_app_service(subscription)
-    get_details_and_output(subscription, all_app_services(subscription).first)
-  end
-
-  # notes
-  # appsettings is blank - may not be possible? - https://github.com/MicrosoftDocs/azure-docs/issues/38698
-  # testing subscription '4c1a8af4-85cb-44c7-9528-491d3848d341'
+class Cli
+  include AzureAppServiceDetails
 
   def output_usage
     puts <<~USAGE
@@ -149,34 +173,25 @@ module AzureAppServiceDetails
 
       Usage:
 
-      Retrieve all App Service's in a given subscription:
-      #{$PROGRAM_NAME} <subscription-id>
-
-
-      Retrieve a single App Service's:
-      #{$PROGRAM_NAME} <subscription-id> <app-service-name>
-
-
-      The output will include a summary to standard output as well as a file written
-      to the new directory in the current working directory in the format of:
-      ./subscription_<subscription-id>_app_services/<app-service-name>_<app-service-resource-group-name>.json
-
-
-      How to Use
-
-      First authentication and set a token with the azure-cli using:
+      First authenticate with Azure and set a token via the azure-cli using:
 
       az login
       az account set --subscription <SUBSCRIPTION_ID>
       export AZURE_TOKEN=$(az account get-access-token --query accessToken --output tsv)
 
-      Run the command as above, for example:
+      Retrieve all App Service's in a given subscription:
+      #{$PROGRAM_NAME} <subscription-id>
 
-      #{$0}  <subscription-id>
+      Retrieve a single App Service's:
+      #{$PROGRAM_NAME} <subscription-id> <app-service-name>
+
+      The output will include a summary to standard output as well as a file written
+      to the new directory in the current working directory in the format of:
+      ./subscription_<subscription-id>_app_services/<app-service-name>_<app-service-resource-group-name>.json
     USAGE
   end
 
-  def cli_execute
+  def execute
     case ARGV.length
     when 1
       output_all_app_services(ARGV[0])
@@ -188,5 +203,4 @@ module AzureAppServiceDetails
   end
 end
 
-include AzureAppServiceDetails
-cli_execute
+Cli.new.execute
