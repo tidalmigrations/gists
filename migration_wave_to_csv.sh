df9fde088cc722f41a830935b4903f3638819d74
samdesmondking <samuel-king@live.com.au>
2025-04-14 21:34:45 +1000

    fix: updated url in unix script

--- migration_wave_to_csv.sh
+++ migration_wave_to_csv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-access_token=$(curl -H Content-Type\:\ application/json -XPOST https\://$subdomain.tidalmg.com/api/v1/authenticate -d \{\"username\"\:\ \"$username\"\,\ \"password\"\:\ \"$password\"\} | jq '.access_token' | sed 's/"//g')
+access_token=$(curl -H Content-Type\:\ application/json -XPOST https\://$subdomain.tidal.cloud/api/v1/authenticate -d \{\"username\"\:\ \"$username\"\,\ \"password\"\:\ \"$password\"\} | jq '.access_token' | sed 's/"//g')
 
-move_group=$(curl -H Authorization\:\ \"Bearer\ $access_token\" -H Content-Type\:\ application/json -XGET https\://$subdomain.tidalmg.com/api/v1/move_groups/$move_group_id | jq '.servers')
+move_group=$(curl -H Authorization\:\ \"Bearer\ $access_token\" -H Content-Type\:\ application/json -XGET https\://$subdomain.tidal.cloud/api/v1/move_groups/$migration_wave_id | jq '.servers')
 
 echo "hostname, username, password, domain"
 echo $move_group | jq -M -r '.[] | .host_name as $h | .custom_fields | [$h, .username, .password, .domain] | @csv'
